235adf7c058c6ad8a268d671d5a8a399944df559
serban001 <43972861+serban001@users.noreply.github.com>
2020-02-12 18:32:15 +0000

    _02

---
+++ Cicibub/Cicibub/Assets/Scripts/Projectile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public float distance;
+    public LayerMask whatIsSolid;
+    
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    private void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
+        if (hitInfo.collider != null)
+        {
+                if (hitInfo.collider.CompareTag("Enemy"))
+                {
+                    Debug.Log("Bine boss");
+                }
+                DestroyProjectile();
+                Debug.Log(hitInfo.collider.name);
+        }
+        
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(this.gameObject);
+    }
+}
