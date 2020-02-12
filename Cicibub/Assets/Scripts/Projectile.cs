5db4b9f37ee3d02e4ed5ab1c015a76f2e4654b57
serban001 <43972861+serban001@users.noreply.github.com>
2020-02-12 17:21:02 +0000

    Merg peretzii

---
+++ Cicibub/Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(this.gameObject);
+    }
+}
